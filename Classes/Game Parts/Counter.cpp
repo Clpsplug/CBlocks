ff4e097bfe0f4df936acfba1c5c42841d1c30e95
Collapsed Plug <hsp.tosh.5200113@gmail.com>
2015-09-01 21:22:05 +0900

    カウンター関係だけはクラス化しておくことにした

---
+++ Classes/Game Parts/Counter.cpp
@@ -0,0 +1,19 @@
+//
+//  Counter.cpp
+//  CBlocks
+//
+//  Created by 有働 篤人 on 9/1/15.
+//
+//
+
+#include "Counter.h"
+
+USING_NS_CC;
+
+Counter::Counter(){
+    
+}
+
+Counter::~Counter(){
+    
+}
